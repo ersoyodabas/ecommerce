9c9450be2eb85ee02f40f6e5033ecf587c39dcf7
Ersoy Odabas <ersoy.odabas@alisanlogictics.com>
2025-03-10 16:57:22 +0300

    next

--- obj/Debug/net8.0/ecommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2d9eca5c3bcee4cd6483968e332157b735d100f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e339214fc0724ab0495f29022bd7e2ddc85fd8e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
